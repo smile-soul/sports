49a4db309ac6142111720c1f6f1021d5eef8eee3
ttop5 <ttop5@qq.com>
2017-10-10 01:00:38 +0800

    feat(Rules): 比赛规则页面基本架构

--- sports/client/src/views/Overview.vue
+++ sports/client/src/views/Overview.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="overview-view">
-    <div class="title" style="text-align: center">
-    </p>欢迎使用运动会编排系统</p>
+    <div class="title">
+      </p>欢迎使用运动会编排系统</p>
     </div>
   </div>
 </template>
 
 
 <script>
+export default {
+  name: 'overview-view',
+}
 </script>
 
 
 <style lang="scss">
 .overview-view {
   .title {
+    text-align: center;
     font-size: 3.7rem;
     font-weight: bold;
     padding-top: 117px;
---
+++ sports/client/src/views/Rules.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="rules-view">
+    <h1>比赛规则</h1>
+    <el-tabs v-model="activeName" type="card">
+      <el-tab-pane label="甲组" name="firstTab">
+        tab01
+      </el-tab-pane>
+      <el-tab-pane label="乙组" name="secondTab">
+        tab02
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'rules-view',
+  data() {
+    return {
+      activeName: 'firstTab',
+    }
+  },
+}
+</script>
+
+
+<style lang="scss">
+.rules-view {
+}
+</style>
