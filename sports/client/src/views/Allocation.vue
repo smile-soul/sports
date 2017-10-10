0b36709dce03b9e6d4942d67caa8a57a54a584c5
ttop5 <ttop5@qq.com>
2017-10-10 22:33:40 +0800

    feat(src/): 左侧菜单及页面架构

---
+++ sports/client/src/views/Allocation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="allocation-view">
+    <h1>号码分配</h1>
+    <el-tabs type="border-card">
+      <el-tab-pane label="自动分配">
+      </el-tab-pane>
+      <el-tab-pane label="手动分配">
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'allocation-view',
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+
+<style lang="scss">
+.allocation-view {
+}
+</style>
