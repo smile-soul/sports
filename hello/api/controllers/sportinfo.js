a1f3fc5173b2f75ee2a5e32906721f3bbecc64f8
smilesoul <517578601@qq.com>
2017-10-12 20:52:03 +0800

    feat: 添加跑到 get put api

--- hello/api/controllers/sportinfo.js
+++ hello/api/controllers/sportinfo.js
@@ -3,7 +3,9 @@
 const util = require('util');
 
 module.exports = {
-  sportinfo: sportBase
+  sportBaseget: sportBaseget,
+  sportBaseput: sportBaseput,
+  sportRoad: sportRoad
 };
 
 /*
@@ -12,7 +14,7 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function sportBase(req, res) {
+function sportBaseget(req, res) {
   pool.getConnection(function(err, connection) {
     // Use the connection
     connection.query('select * from sportInfo', function (error, results, fields) {
@@ -21,4 +23,34 @@ function sportBase(req, res) {
       if (error) throw error;
     });
   });
+}
+
+function sportBaseput(req, res) {
+  const bodyname = Object.values(req.swagger.params.body.value);
+  pool.getConnection(function(err, connection) {
+    connection.query(`update sportInfo set
+    sportname=${getjson(bodyname[0])},
+    sportBeginTime=${getjson(bodyname[1])},
+    sportEndTime=${getjson(bodyname[2])},
+    sportday=${getjson(bodyname[3])}
+    where id=1`, function (error, results, fields) {
+      res.send(results);
+      connection.release();
+      if (error) throw error;
+    });
+  });
+}
+
+function sportRoad(req, res) {
+  pool.getConnection(function(err, connection) {
+    connection.query(`select * from sportRoad`, function (error, results, fields) {
+      res.send(results);
+      connection.release();
+      if (error) throw error;
+    });
+  });
+}
+
+function getjson(strings) {
+  return JSON.stringify(strings);
 }
