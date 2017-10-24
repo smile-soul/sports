3573d0af6608080c8171d35d141301e98a31c520
smile <517578601@qq.com>
2017-10-24 22:37:55 +0800

    feat: 添加授权认证

--- oauth/server.js
+++ oauth/server.js
@@ -3,26 +3,18 @@ const passport = require('passport');
 const Strategy = require('passport-http-bearer').Strategy;
 const db = require('./db');
 const app = express();
-// Configure the Bearer strategy for use by Passport.
-//
-// The Bearer strategy requires a `verify` function which receives the
-// credentials (`token`) contained in the request.  The function must invoke
-// `cb` with a user object, which will be set at `req.user` in route handlers
-// after authentication.
+
 passport.use(new Strategy(
   function(token, cb) {
     db.users.findByToken(token, function(err, user) {
-      if (err) { return cb(err); }
+      if (err) { return 'cb(err)'; }
       if (!user) { return cb(null, false); }
       return cb(null, user);
     });
   }));
 
 
-// Create a new Express application.
 app.use(express.static(__dirname+ '/view'));
-
-// Configure Express application.
 app.use(require('morgan')('combined'));
 
 // curl -v -H "Authorization: Bearer 123456789" http://127.0.0.1:3000/
@@ -30,7 +22,7 @@ app.use(require('morgan')('combined'));
 app.get('/',
   passport.authenticate('bearer', { session: false }),
   function(req, res) {
-    res.render('index.html');
+    res.redirect('http://localhost:8080/')
   });
 
 app.listen(3000);
