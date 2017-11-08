bbb15fae818a5bca7f0220631c3603b2db1237fd
smile <517578601@qq.com>
2017-11-08 21:16:22 +0800

    feat: 修改api

--- newserver/controllers/baseinfo.js
+++ newserver/controllers/baseinfo.js
@@ -10,10 +10,9 @@ module.exports = {
 
 function sportsAllMethod(req, res) {
   pool.getConnection(function (err, connection) {
-    // Use the connection
-    connection.query('select * from bend', function (error, results, fields) {
-      console.log(results);
-      res.json(results);
+    connection.query('select method from method', function (error, results, fields) {
+      const arrayMethod = getArray(results, 'method');
+      res.json(arrayMethod);
       connection.release();
       if (error) throw error;
     });
@@ -21,24 +20,34 @@ function sportsAllMethod(req, res) {
 }
 
 function sportsAllStraight(req, res) {
-  const bodyname = req.swagger.params.body.value;
   pool.getConnection(function (err, connection) {
     // Use the connection
-    connection.query('select * from bend', function (error, results, fields) {
-      console.log(results);
-      res.json(results);
-      connection.release();
+    connection.query('select road from straight', function (error, results, fields) {
+      const arrayStraight = getArray(results, 'road');
+      console.log(arrayStraight);
       if (error) throw error;
     });
   });
 }
 function sportsAllBend(req, res) {
-  const bodyname = req.swagger.params.body.value;
   pool.getConnection(function (err, connection) {
-
+    connection.query('select road from bend', function (error, results, fields) {
+      const arrayBend = getArray(results, 'road');
+      res.json(arrayBend);
+      connection.release();
+      if (error) throw error;
+    });
   });
 }
-
+//转换字符串
 function getjson(strings) {
   return JSON.stringify(strings);
+}
+// 获取数据数组
+function getArray(arrayData, type) {
+  const newArray = [];
+  arrayData.map( value => {
+    newArray.push(value[type]);
+  })
+  return newArray;
 }
